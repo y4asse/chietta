f77e66fdf572c3c36bf0d6caab5a11e7fe29a949
y4asse <yasse0218@icloud.com>
2023-10-10 16:48:45 +0900

    dbとredisからのpost取得を分ける

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -4,40 +4,51 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
   // redisから取得
-  getPosts: publicProcedure.query(async ({ ctx }) => {
-    const { kv } = ctx;
+  getPostsFromDb: publicProcedure.query(async ({ ctx }) => {
     const take = 20;
     const startTimeline = Date.now();
-    const timeline: string[] = await kv.zrange("timeline", 0, take - 1);
     const posts = await ctx.db.post.findMany({
       orderBy: { createdAt: "desc" },
       take,
     });
     const endTimeline = Date.now();
     console.log(
-      "[getPosts] get timeline = " + (endTimeline - startTimeline) + "ms",
+      "[getPostsFromDb] get posts = " + (endTimeline - startTimeline) + "ms",
     );
-    // if (timeline.length === 0) return [];
-    // const pipeline = kv.pipeline();
-    // timeline.map((id) => {
-    //   pipeline.hgetall(`post:${id}`);
-    // });
-    // const startExec = Date.now();
-
-    // const result: {
-    //   title: string;
-    //   url: string;
-    //   createdAt: string;
-    //   provider: string;
-    // }[] = await pipeline.exec();
-    // const endExec = Date.now();
-    // console.log("[getPosts] exec = " + (endExec - startExec) + "ms");
-    // const posts = result.map((post, i) => ({
-    //   ...post,
-    //   id: timeline[i]!,
-    //   createdAt: new Date(post.createdAt),
-    // }));
 
     return posts;
+  }),
+
+  getPostsFromRedis: publicProcedure.query(async ({ ctx }) => {
+    const { kv } = ctx;
+    const take = 20;
+    const startTimeline = Date.now();
+    const timeline: string[] = await kv.zrange("timeline", 0, take - 1);
+    const endTimeline = Date.now();
+    console.log(
+      "[getPosts] get timeline = " + (endTimeline - startTimeline) + "ms",
+    );
+    if (timeline.length === 0) return [];
+    const pipeline = kv.pipeline();
+    timeline.map((id) => {
+      pipeline.hgetall(`post:${id}`);
+    });
+    const startExec = Date.now();
+
+    const result: {
+      title: string;
+      url: string;
+      createdAt: string;
+      provider: string;
+    }[] = await pipeline.exec();
+    const endExec = Date.now();
+    console.log("[getPostsFromRedis] exec = " + (endExec - startExec) + "ms");
+    const posts = result.map((post, i) => ({
+      ...post,
+      id: timeline[i]!,
+      createdAt: new Date(post.createdAt),
+    }));
+
+    return posts as Post[];
   }),
 });
