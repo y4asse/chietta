acf816d29a2b2e28d218efb10964e775e5466f8a
y4asse <yasse0218@icloud.com>
2023-10-15 11:27:45 +0900

    add page

---
+++ app/latest/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Latest = () => {
+  return <div>Latest</div>
+}
+
+export default Latest
--- components/tech/Posts.tsx
+++ components/tech/Posts.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import PostItem from './PostItem'
-import { Post } from '@prisma/client'
+import { ReturnPost } from '@/app/api/post/route'
 
-const Posts = ({ posts }: { posts: Post[] }) => {
+const Posts = ({ posts }: { posts: ReturnPost[] }) => {
   return (
     <div className="flex mx-auto flex-wrap p-5  gap-10 max-w-[800px] mt-10">
       {posts.map((post) => (
