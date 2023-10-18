4778e8f499a4cbcf6cd2ba24e858ed651f838fe5
y4asse <yasse0218@icloud.com>
2023-10-18 15:06:46 +0900

    検索ワードが同じの時のローディングをなくす

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default async function Home() {
-  return <div>hell0</div>
+import WrapContainer from '@/components/layout/WrapContainer'
+import ScrollDetect from '@/components/scroll/ScrollDetect'
+import Posts from '@/components/tech/Posts'
+import { getLatestPosts } from '@/server/getPosts'
+
+const Home = async () => {
+  const offset = 0
+  const posts = await getLatestPosts(offset)
+  if (!posts) return <WrapContainer>error</WrapContainer>
+  return (
+    <main className="min-h-screen min-w-[300px] bg-[#fffafa] items-center py-10">
+      <h1 className="text-center text-3xl font-bold my-5">トレンド記事</h1>
+      <ScrollDetect type="latest" q="">
+        <Posts posts={posts} />
+      </ScrollDetect>
+    </main>
+  )
 }
+
+export default Home
