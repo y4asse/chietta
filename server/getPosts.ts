2b702bd64c3afae54d06bcdf217bd189e7daf8b4
y4asse <yasse0218@icloud.com>
2023-10-20 11:25:38 +0900

    トレンド機能

--- server/getPosts.ts
+++ server/getPosts.ts
@@ -41,3 +41,23 @@ export const getSearchPosts = async (offset: number, q: string): Promise<ReturnP
     return null
   }
 }
+
+export const getTrends = async (offset: number, q?: string): Promise<ReturnPost[] | null> => {
+  try {
+    const queryURL = new URL(`${process.env.NEXT_PUBLIC_API_URL}/trends?offset=${offset}`)
+    const res = await fetch(queryURL, {
+      next: { revalidate: 5 * 60 }
+    })
+    if (!res.ok) {
+      console.log('Http Error')
+      console.log('status: ' + res.status)
+      console.log('statusText: ' + res.statusText)
+      throw new Error(`HTTP error! Status: ${res.statusText}`)
+    }
+    const posts = (await res.json()) as ReturnPost[]
+    return posts
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
