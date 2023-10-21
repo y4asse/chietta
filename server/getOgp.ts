2ffcdc681ff5129593d42bcaa443087def93a90f
y4asse <yasse0218@icloud.com>
2023-10-21 11:04:34 +0900

    delete ogp time

--- server/getOgp.ts
+++ server/getOgp.ts
@@ -1,13 +1,10 @@
 import { OgObject } from 'open-graph-scraper/dist/lib/types'
 
 export const getOgp = async (url: string) => {
-  const startTime = Date.now()
   try {
     const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ogp?url=${url}`, {
       cache: 'force-cache'
     }).then((res) => res.json() as OgObject)
-    const endTime = Date.now()
-    console.log('[getOgp] get ogp = ' + (endTime - startTime) + 'ms')
     return result
   } catch (error) {
     return null
