28a0305d7e76c854549ad8153961c02da49490cf
y4asse <yasse0218@icloud.com>
2023-10-23 21:10:30 +0900

    getPostsのラップ

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,11 +1,15 @@
 import WrapContainer from '@/components/layout/WrapContainer'
 import ScrollDetect from '@/components/scroll/ScrollDetect'
 import Posts from '@/components/tech/Posts'
-import { getLatestCompanyPosts } from '@/server/getPosts'
+import { getPost } from '@/server/getPosts'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
 
 const CompanyLatest = async () => {
+  const data = await getServerSession(authOptions)
+  const userId = data ? data.user.id : null
   const offset = 0
-  const posts = await getLatestCompanyPosts(offset)
+  const posts = await getPost('company', { offset, userId })
   if (!posts)
     return (
       <WrapContainer>
