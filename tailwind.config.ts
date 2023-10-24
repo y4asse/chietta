b6ffa9c0c495e238e50d0912c6a09ff53940ea59
y4asse <yasse0218@icloud.com>
2023-10-24 22:09:44 +0900

    colorの追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,8 @@ const config: Config = {
     colors: {
       primary: '#FF8383',
       gray: '#65717b',
-      pink: '#fff2f2'
+      pink: '#fff2f2',
+      main: '#fffafa'
     },
     extend: {
       backgroundImage: {
