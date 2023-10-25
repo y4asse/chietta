a934266c05fdf269a54620e47f8c5df10d2231e3
y4asse <yasse0218@icloud.com>
2023-10-25 19:58:10 +0900

    middlewareの追加

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+export default withAuth(function middleware(req) {
+  const { pathname } = req.nextUrl
+
+  //   console.log(req.nextauth.token)
+  //   // 管理者権限のないユーザーが管理者権限が必要なパスにアクセスした場合は404
+  //   if (req.nextauth.token === null) {
+  //     return NextResponse.rewrite(new URL('/404', req.url))
+  //   }
+})
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/mypage/:path*'
+}
