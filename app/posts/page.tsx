519634ebec0de74a632d0fcc2ddff4edca6dd7a6
y4asse <yasse0218@icloud.com>
2023-10-25 20:59:14 +0900

    addOgpにnullを許可

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,12 +3,13 @@ import { addOgp } from '@/server/addOgp'
 import { db } from '@/server/db'
 import React from 'react'
 
+//このページもキャッシュしたい
 const Post = async () => {
   const result = await db.userPost.findMany({
     orderBy: { createdAt: 'desc' },
     include: { user: true }
   })
-  const userPosts = await addOgp(result)
+  const userPosts = await addOgp(result, { allowNull: true })
   return (
     // <ScrollDetect type="userPosts" q="">
     <UserPosts userPosts={userPosts} />
