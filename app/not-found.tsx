4f7c38bad67ded6c48ad4562157c25f2da3e7ea2
y4asse <yasse0218@icloud.com>
2023-10-26 22:44:58 +0900

    fix bug not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className="text-center my-[140px] font-bold">
+    <div className="text-center py-[140px] font-bold">
       <h1 className="text-3xl">お探しのページが見つかりません</h1>
       <Image className="mx-auto" src="/img/cat.png" alt="猫の画像" width={300} height={300} />
     </div>
