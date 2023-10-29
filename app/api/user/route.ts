ce639fe0e2ca3ed2020fca05fb9919fc7df2b4e2
y4asse <yasse0218@icloud.com>
2023-10-29 23:14:24 +0900

    user api getの追加

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,32 @@
+import { addOgp } from '@/server/addOgp'
 import { db } from '@/server/db'
 import { getToken } from 'next-auth/jwt'
 import { NextRequest } from 'next/server'
 import z from 'zod'
+
+// 誰でも見れる
+export const GET = async (req: NextRequest) => {
+  const userId = req.nextUrl.searchParams.get('user_id')
+  if (userId === null) {
+    return Response.json({ message: '不正なリクエスト' }, { status: 400 })
+  }
+  const user = await db.user.findUnique({
+    where: { id: userId },
+    include: {
+      UserPost: {
+        orderBy: {
+          createdAt: 'desc'
+        }
+      }
+    }
+  })
+  if (user === null) {
+    return Response.json({ message: 'ユーザが見つかりません' }, { status: 404 })
+  }
+  const posts = user.UserPost
+  const postsWithOgp = await addOgp(posts, { allowNull: true })
+  return Response.json({ user, postsWithOgp })
+}
 
 export const PUT = async (req: NextRequest) => {
   const schema = z.object({
