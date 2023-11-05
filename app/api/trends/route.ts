f2585bd2eb37304350c47a8f2ea9053b31e54734
y4asse <yasse0218@icloud.com>
2023-11-05 18:27:17 +0900

    node cacheの利用

--- app/api/trends/route.ts
+++ app/api/trends/route.ts
@@ -1,19 +1,28 @@
 import { addOgp } from '@/server/addOgp'
-import { Trends } from '@prisma/client'
+import { db } from '@/server/db'
 import { NextRequest } from 'next/server'
+import NodeCache from 'node-cache'
 
-export const revalidate = 60 * 60 // 1時間
+const cache = new NodeCache({ stdTTL: 60 * 30 }) // 30分
 
 export const GET = async (req: NextRequest) => {
   const offsetString = req.nextUrl.searchParams.get('offset')
   const offset = offsetString ? parseInt(offsetString) : 0 // 不正な値の時0になる
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/trendsFromDb?offset=${offset}`, {
-    next: { revalidate: 60 * 30 } // 30分
-  })
-  if (!res.ok) {
-    return Response.json({ message: 'サーバーエラー' }, { status: 500 })
+  const cacheKey = 'trends' + offset
+  const cachedPosts = cache.get(cacheKey)
+  if (cachedPosts) {
+    return Response.json(cachedPosts)
   }
-  const articles = (await res.json()).result as Trends[]
+  const startTime = Date.now()
+  const take = 10
+  const articles = await db.trends.findMany({
+    orderBy: { likedCount: 'desc' },
+    take,
+    skip: offset
+  })
+  const endTime = Date.now()
+  console.log('[getPostsFromDB] get trends from DB = ' + (endTime - startTime) + 'ms')
   const returnPosts = await addOgp(articles)
+  cache.set(cacheKey, returnPosts)
   return Response.json(returnPosts)
 }
