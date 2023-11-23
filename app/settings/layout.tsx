8c6a7459d12610036606e7d86ea5acc632e3948b
y4asse <yasse0218@icloud.com>
2023-11-23 18:28:39 +0900

    線消す

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -5,7 +5,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="min-h-screen min-w-[300px] bg-[white] items-center py-10 max-w-[1000px] mx-auto px-3">
       <h1 className="text-center text-3xl font-bold my-5">ユーザ設定</h1>
       <SettingTab />
-      <hr className="text-[#afafaf]" />
       {children}
     </main>
   )
