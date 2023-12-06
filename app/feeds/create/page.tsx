809f9440996d7f4f55131693323ae8b77986b704
y4asse <yasse0218@icloud.com>
2023-12-06 20:37:06 +0900

    新規feedの登録

---
+++ app/feeds/create/page.tsx
@@ -0,0 +1,13 @@
+import FeedLists from '@/components/feeds/list/FeedLists'
+import Input from '@/components/feeds/create/Input'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="max-w-[1000px] mx-auto px-5 py-5 shadow bg-[white] rounded-xl">
+      <Input />
+    </div>
+  )
+}
+
+export default Page
