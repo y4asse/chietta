dc1d5ce3f145b70f6cd9e6d4ffc5788b6f239d49
y4asse <yasse0218@icloud.com>
2023-12-07 23:27:03 +0900

    余計な行を削除

--- app/feeds/list/[page]/page.tsx
+++ app/feeds/list/[page]/page.tsx
@@ -31,7 +31,6 @@ const Page = async ({ params }: { params: { page: string } }) => {
   const [feeds, totalCount] = await Promise.all([getFeeds, getTotalCount])
   const totalPageCount = Math.floor(totalCount / take) + 1
   if (!feeds || totalPageCount < page + 1) return notFound()
-  if (totalCount / take < page) return notFound()
   return (
     <div className="max-w-[1000px] mx-auto px-1">
       <h1 className="text-center text-3xl font-bold my-5">フィード一覧</h1>
