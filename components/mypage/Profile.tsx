f96fce36a5acb7c63b956242c998ff4eff2ff4e1
y4asse <yasse0218@icloud.com>
2023-12-13 01:47:57 +0900

    blankから_blankに変更

--- components/mypage/Profile.tsx
+++ components/mypage/Profile.tsx
@@ -30,27 +30,27 @@ const Profile = ({ user, sessionUser }: { user: User; sessionUser: SessionUser |
         )}
         <div className="flex items-center gap-5 mt-5 text-2xl">
           {user.x && (
-            <a target="blank" href={`https://x.com/${user.x}`}>
+            <a target="_blank" href={`https://x.com/${user.x}`}>
               <FaXTwitter />
             </a>
           )}
           {user.github && (
-            <a target="blank" href={`https://github.com/${user.github}`}>
+            <a target="_blank" href={`https://github.com/${user.github}`}>
               <FaGithub />
             </a>
           )}
           {user.zenn && (
-            <a target="blank" href={`https://zenn.dev/${user.zenn}`}>
+            <a target="_blank" href={`https://zenn.dev/${user.zenn}`}>
               <Zenn />
             </a>
           )}
           {user.qiita && (
-            <a target="blank" href={`https://qiita.com/${user.qiita}`}>
+            <a target="_blank" href={`https://qiita.com/${user.qiita}`}>
               <Qiita />
             </a>
           )}
           {user.note && (
-            <a target="blank" href={`https://note.com/${user.note}`}>
+            <a target="_blank" href={`https://note.com/${user.note}`}>
               <Note />
             </a>
           )}
