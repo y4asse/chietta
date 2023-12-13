9081ed32a86686989fe7b9fd4d8450888187902f
y4asse <yasse0218@icloud.com>
2023-12-13 12:42:37 +0900

    ユーザページのレイアウト修正

--- app/user/[id]/layout.tsx
+++ app/user/[id]/layout.tsx
@@ -1,5 +1,10 @@
+import Profile from '@/components/mypage/Profile'
+import UserTab from '@/components/user/UserTab'
+import { authOptions } from '@/server/auth'
 import { db } from '@/server/db'
 import { Metadata } from 'next'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const user = await db.user.findUnique({
@@ -13,8 +18,35 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   }
 }
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>
+const Layout = async ({ children, params }: { children: React.ReactNode; params: { id: string } }) => {
+  const session = await getServerSession(authOptions)
+  const sessionUser = session ? session.user : null
+  const { id } = params
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      UserPost: {
+        include: {
+          _count: {
+            select: { like: true }
+          },
+          user: true
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      }
+    }
+  })
+  if (!user) return notFound()
+  return (
+    <div className="pt-10 min-h-screen">
+      <Profile user={user} sessionUser={sessionUser} />
+      <hr className="max-w-[1000px] mx-auto text-lightGray px-5 w-[95%]" />
+      <UserTab id={id} />
+      {children}
+    </div>
+  )
 }
 
 export default Layout
--- components/mypage/Profile.tsx
+++ components/mypage/Profile.tsx
@@ -2,7 +2,6 @@
 
 import { User } from '@prisma/client'
 import { User as SessionUser } from 'next-auth'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { FaXTwitter, FaGithub } from 'react-icons/fa6'
@@ -12,13 +11,13 @@ import Qiita from '../icons/Qiita'
 
 const Profile = ({ user, sessionUser }: { user: User; sessionUser: SessionUser | null }) => {
   return (
-    <div className="max-w-[800px] w-full mx-auto flex flex-wrap p-3 mb-5 gap-5">
+    <div className="max-w-[1000px] w-full mx-auto flex flex-wrap p-3 mb-5 gap-5">
       <img
         src={user.image!}
         alt="ユーザアイコン"
         className="w-[70px] h-[70px] md:w-[100px] md:h-[100px] rounded-full mx-auto border border-[#afafaf]"
       />
-      <div className="w-full md:w-1/2">
+      <div className="w-full md:w-2/3">
         <h1 className="text-2xl font-bold">{user.name}</h1>
         <p className="mt-5 text-gray">{user.introduction ?? '自己紹介がありません'}</p>
         {sessionUser && user.id === sessionUser.id && (
