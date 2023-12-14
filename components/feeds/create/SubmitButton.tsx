065fa58aa200dff93f3b1ffdd0d2f62ad36f864b
y4asse <yasse0218@icloud.com>
2023-12-14 14:29:56 +0900

    submitボタンのコンポーネント化

--- components/feeds/create/SubmitButton.tsx
+++ components/feeds/create/SubmitButton.tsx
@@ -1,13 +1,15 @@
+'use client'
+
 import { useFormStatus } from 'react-dom'
 
-const SubmitButton = () => {
+const SubmitButton = ({ text, pendingText }: { text: string; pendingText: string }) => {
   const { pending } = useFormStatus()
   return (
     <input
       disabled={pending}
       role="button"
       type="submit"
-      value={pending ? '登録中...' : 'フィードを登録'}
+      value={pending ? pendingText : text}
       className={`rounded bg-primary text-[white] px-3 py-1 font-semibold ${pending && 'cursor-not-allowed'}`}
     />
   )
