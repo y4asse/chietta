2188aeaec51261cf7c452d58ace30cc29e68242f
y4asse <yasse0218@icloud.com>
2024-01-01 17:17:22 +0900

    タイムラインをフォロー中のユーザの投稿に変更

--- components/userPost/UserPostsServer.tsx
+++ components/userPost/UserPostsServer.tsx
@@ -1,10 +1,16 @@
+import { getFollowingUserPosts } from '@/app/posts/_actions/actions'
 import WrapContainer from '@/components/layout/WrapContainer'
 import UserPosts from '@/components/userPost/UserPosts'
-import { getUserPosts } from '@/server/getUserPosts'
+import { authOptions } from '@/server/auth'
+import { getServerSession } from 'next-auth'
+import NoContent from '../noContent/NoContent'
 
 const UserPostsServer = async () => {
-  const userPosts = await getUserPosts() //　エラー時nullが返る
-  if (!userPosts) return <WrapContainer>エラーが発生しました</WrapContainer>
+  const session = await getServerSession(authOptions)
+  const { userPosts, error } = await getFollowingUserPosts(session!.user.id) //　エラー時nullが返る
+  if (error || !userPosts) return <WrapContainer>エラーが発生しました</WrapContainer>
+  if (userPosts.length === 0) return <NoContent text="フォローしているユーザの投稿がありません" />
+  console.log(userPosts)
   return <UserPosts userPosts={userPosts} />
 }
 
