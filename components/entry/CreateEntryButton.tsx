52303c01deff3cd2855c8f487ab665ede85c2005
y4asse <yasse0218@icloud.com>
2024-01-06 02:32:03 +0900

    未ログイン時の処理を追加

--- components/entry/CreateEntryButton.tsx
+++ components/entry/CreateEntryButton.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { createEntry } from '@/app/entry/[url]/_actions/actions'
+import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
 const CreateEntryButton = ({ url, title, image }: { url: string; title: string | null; image: string | null }) => {
   const router = useRouter()
+  const { data: session } = useSession()
   const handleClick = async () => {
+    if (!session)
+      return router.push(`/login?callbackUrl=${process.env.NEXT_PUBLIC_FRONT_URL}/entry/${encodeURIComponent(url)}`)
     const { error } = await createEntry({ url, title, image })
     if (error) {
       return alert(error)
