8f4aadee39e9b8b51cf8b05859ededf24e8344c6
y4asse <yasse0218@icloud.com>
2024-01-07 17:13:42 +0900

    no-contentの表示

--- components/timeline/FollowingActivityList.tsx
+++ components/timeline/FollowingActivityList.tsx
@@ -1,15 +1,17 @@
 import { getUserActivities } from '@/server/timeline/getUserActivities'
 import ActivityItem from './ActivityItem'
 import NoContent from '@/components/error/NoContent'
+import ErrorComponent from '../error/ErrorComponent'
 
 const FollowingUserActivityList = async () => {
   const activities = await getUserActivities()
+  if (!activities) return <ErrorComponent />
   return (
     <div className="max-w-[1000px] mx-auto">
-      {activities ? (
+      {activities?.length > 0 ? (
         activities.map((activity) => <ActivityItem key={activity.id} activity={activity} />)
       ) : (
-        <NoContent text="アクティビティがありません" />
+        <NoContent text="フォロー中のユーザのアクティビティがありません" />
       )}
     </div>
   )
