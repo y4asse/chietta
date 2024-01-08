d1e042c3e9e4683ea92d803e6003ec2a5469166e
y4asse <yasse0218@icloud.com>
2024-01-08 22:20:38 +0900

    エントリーのnotFound

--- app/entry/page.tsx
+++ app/entry/page.tsx
@@ -1,7 +1,8 @@
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
 const Page = ({ searchParams }: { searchParams: { url: string } }) => {
   const { url } = searchParams
+  if (!url) notFound()
   return redirect(`/entry/${encodeURIComponent(url)}`)
 }
 
