2d0a0eed7434977eb1f3a223a7516e92cf3e6489
y4asse <yasse0218@icloud.com>
2024-01-10 00:40:23 +0900

    奇数個の時のレイアウト修正

--- components/tech/Posts.tsx
+++ components/tech/Posts.tsx
@@ -6,16 +6,14 @@ import { motion } from 'framer-motion'
 import { PostItemType } from '@/types/postItem'
 
 const Posts = ({ posts }: { posts: PostItemType[] }) => {
-  if (posts.length === 0) return <></>
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className="flex mx-auto flex-wrap p-5 gap-10 max-w-[800px]"
-    >
-      {posts.map((post) => (
-        <PostItem post={post} key={post.url} />
-      ))}
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <div className="flex mx-auto flex-wrap p-5 gap-10 max-w-[800px]">
+        {posts.map((post) => (
+          <PostItem post={post} key={post.url} />
+        ))}
+        {posts.length % 2 === 1 && <div className="w-[340px] h-0" />}
+      </div>
     </motion.div>
   )
 }
