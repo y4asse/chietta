e033af01e004eec802ecf5df9e76911ef30dbd0c
y4asse <yasse0218@icloud.com>
2024-01-09 22:37:33 +0900

    public apiの仮作成

---
+++ app/api/v2/[id]/bookmark/route.tsx
@@ -0,0 +1,35 @@
+import { getBookmark } from '@/server/api/user/bookmark/getBookmark'
+import { getUser } from '@/server/userPage/getUser'
+import { NextRequest } from 'next/server'
+
+export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
+  const { id } = params
+
+  //pageの取得
+  const page = req.nextUrl.searchParams.get('page')
+  const pageInt = parseInt(page || '0')
+  if (isNaN(pageInt)) {
+    return Response.json({ error: 'page is not number' }, { status: 400 })
+  }
+  console.log(pageInt)
+  if (pageInt < 0 || pageInt > 1000) {
+    return Response.json({ error: 'page is out of range' }, { status: 400 })
+  }
+  if (!id) {
+    return Response.json({ error: 'user id is required' }, { status: 400 })
+  }
+
+  const user = await getUser(id)
+  if (!user) {
+    return Response.json({ error: 'user not found' }, { status: 404 })
+  }
+
+  const take = 100
+  const bookmark = await getBookmark({ idCreatedByUser: id, take, page: pageInt })
+  if (!bookmark) {
+    return Response.json({ error: 'internal server error' }, { status: 404 })
+  }
+  const data = bookmark
+  console.log(data)
+  return Response.json(data)
+}
--- server/userPage/getUser.ts
+++ server/userPage/getUser.ts
@@ -4,24 +4,30 @@ import { db } from '../db'
 export type UserType = Prisma.PromiseReturnType<typeof getUser>
 
 export const getUser = async (idCreatedByUser: string) => {
-  const user = await db.user.findUnique({
-    where: { idCreatedByUser },
-    select: {
-      Followers: true,
-      Follow: true,
-      idCreatedByUser: true,
-      id: true,
-      name: true,
-      image: true,
-      introduction: true,
-      x: true,
-      github: true,
-      zenn: true,
-      qiita: true,
-      note: true,
-      hatena: true,
-      webSite: true
-    }
-  })
-  return user
+  try {
+    const user = await db.user.findUnique({
+      where: { idCreatedByUser },
+      select: {
+        Followers: true,
+        Follow: true,
+        idCreatedByUser: true,
+        id: true,
+        name: true,
+        image: true,
+        introduction: true,
+        x: true,
+        github: true,
+        zenn: true,
+        qiita: true,
+        note: true,
+        hatena: true,
+        webSite: true
+      }
+    })
+    return user
+  } catch (e) {
+    console.log('Error in getUser')
+    console.error(e)
+    return null
+  }
 }
