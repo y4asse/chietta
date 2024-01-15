69a6b66ee07236c39b5b45627a26c71e4d3862a0
y4asse <yasse0218@icloud.com>
2024-01-15 19:54:02 +0900

    ビルドエラー修正

--- app/_actions/actions.ts
+++ app/_actions/actions.ts
@@ -3,6 +3,7 @@
 import { authOptions } from '@/server/auth'
 import { db } from '@/server/db'
 import { getOgp } from '@/server/getOgp'
+import { getHashedUrl } from '@/utils/getHashedUrl'
 import { getServerSession } from 'next-auth'
 
 export const createBookmarkByUrl = async ({ url }: { url: string }) => {
@@ -13,6 +14,7 @@ export const createBookmarkByUrl = async ({ url }: { url: string }) => {
     const session = await getServerSession(authOptions)
     if (!session) throw new Error('権限がありません')
     const user_id = session.user.id
+    const hashedUrl = await getHashedUrl(url)
     const bookmark = await db.bookmark.create({
       data: {
         user: {
@@ -22,8 +24,8 @@ export const createBookmarkByUrl = async ({ url }: { url: string }) => {
         },
         entry: {
           connectOrCreate: {
-            where: { url },
-            create: { url, title, image, user: { connect: { id: user_id } } }
+            where: { hashed_url: hashedUrl },
+            create: { url, hashed_url: hashedUrl, title, image, user: { connect: { id: user_id } } }
           }
         }
       }
@@ -55,9 +57,10 @@ export const deleteBookmarkByUrl = async ({ url }: { url: string }) => {
     const session = await getServerSession(authOptions)
     if (!session) throw new Error('権限がありません')
     const user_id = session.user.id
+    const hashedUrl = await getHashedUrl(url)
     const entry = await db.entry.findUnique({
       where: {
-        url: url
+        hashed_url: hashedUrl
       }
     })
     if (!entry) throw new Error('エントリーが見つかりません')
