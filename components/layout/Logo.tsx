a52cc437a205e28aa5c0f2722d85f01df98474fa
y4asse <yasse0218@icloud.com>
2024-01-16 01:00:04 +0900

    正月終わり

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -6,16 +6,7 @@ const Logo = () => {
     <div className="text-3xl font-extrabold flex items-center gap-2">
       <Image src={'/img/catCircle.png'} alt="Chiettaのロゴ画像" width={200} height={200} className="w-[30px]" />
 
-      <span className="relative">
-        Chietta
-        <Image
-          src={'/img/take.png'}
-          alt="Chiettaのロゴ画像"
-          width={200}
-          height={200}
-          className="absolute w-[30px] top-0 left-full"
-        />
-      </span>
+      <span className="relative">Chietta</span>
     </div>
   )
 }
