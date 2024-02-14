387da441cd25dc928cbe3840439daf755bc5867d
y4asse <yasse0218@icloud.com>
2024-02-14 23:33:54 +0900

    レイアウト修正

--- components/entry/CreateEntry.tsx
+++ components/entry/CreateEntry.tsx
@@ -7,7 +7,7 @@ const CreateEntry = async ({ url }: { url: string }) => {
   const title = ogp ? (ogp.ogTitle ? ogp.ogTitle : null) : null
   const image = ogp ? (ogp.ogImage ? ogp.ogImage[0].url : null) : null
   return (
-    <div className="dark:bg-lightDark pt-5 text-white">
+    <div className="dark:bg-lightDark pt-5 dark:text-white">
       <div className="mx-auto max-w-[1000px] px-3 pb-10 min-h-[calc(100vh-320px)]">
         <EntryInfo url={url} title={title} image={image} />
         <hr className="text-lightGray mt-5" />
