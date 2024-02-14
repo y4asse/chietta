a1dcd0dcf7cd34b702f3f30f64f291ba2d4abb46
y4asse <yasse0218@icloud.com>
2024-02-14 23:01:30 +0900

    コメント欄のレイアウト修正

--- components/entry/CommentTitle.tsx
+++ components/entry/CommentTitle.tsx
@@ -2,23 +2,21 @@ import { FaRegComment } from 'react-icons/fa'
 import Image from 'next/image'
 const CommentTitle = ({ exist }: { exist: boolean }) => {
   return (
-    <div className="border-b border-lightGray dark:border-gray py-3">
+    <div className="border-b border-lightGray dark:border-gray pt-3">
       <div className="flex items-center justify-center text-center font-bold md:text-xl gap-3 text-lg">
         コメント
         <FaRegComment />
       </div>
       {!exist && (
-        <div>
+        <div className="dark:bg-white pb-3">
           <Image
             src="/img/discussion.png"
             width={300}
             height={300}
             alt={'discussionの画像'}
-            className="mx-auto dark:bg-white rounded-2xl my-3"
+            className="mx-auto rounded-2xl my-3"
           />
-          <p className="text-gray dark:text-lightGray text-center mt-1">
-            まだコメントがありません。 最初のコメントをしてみましょう！
-          </p>
+          <p className="text-gray text-center mt-1">まだコメントがありません。 最初のコメントをしてみましょう！</p>
         </div>
       )}
     </div>
