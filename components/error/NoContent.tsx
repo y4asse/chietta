32f745a93e537cf42c105649b5cf916aa443c30d
y4asse <yasse0218@icloud.com>
2024-02-15 13:14:07 +0900

    レイアウト修正

--- components/error/NoContent.tsx
+++ components/error/NoContent.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
-const NoTimeline = ({ text }: { text: string }) => {
+const NoContent = ({ text }: { text: string }) => {
   return (
-    <div>
-      <h1 className="text-center mt-10 text-xl font-bold">{text}</h1>
+    <div className="dark:bg-lightDark dark:text-white min-h-screen pt-20">
+      <h1 className="text-center text-xl font-bold">{text}</h1>
       <Image src="/img/cat.png" alt="猫の画像" width={300} height={300} className="mx-auto" />
     </div>
   )
 }
 
-export default NoTimeline
+export default NoContent
